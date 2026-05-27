31aab6019bea203123c9f330cdd583c193eee470
sebastian <scsebastian1406@gmail.com>
2026-05-27 18:12:11 -0500

    clase calculadora

---
+++ src/caso1/Calculadora.java
@@ -0,0 +1,67 @@
+package caso1;
+import java.util.Scanner;
+
+public class Calculadora {
+	public static void main(String[] args) {
+		
+	Scanner teclado = new Scanner(System.in);
+
+	System.out.print("Numero uno: ");
+    int num1 = teclado.nextInt();
+
+    System.out.print("Numero dos: ");
+    int num2 = teclado.nextInt();
+
+    System.out.println("Elija una operación: +, -, *, /");
+    char operacion = teclado.next().charAt(0);
+
+    double resultado = 0;
+    boolean operacionValida = true;
+
+    switch (operacion) {
+        case '+':
+            resultado = sumar(num1, num2);
+            break;
+        case '-':
+            resultado = restar(num1, num2);
+            break;
+        case '*':
+            resultado = multiplicar(num1, num2);
+            break;
+        case '/':
+            if (num2 != 0) {
+                resultado = dividir(num1, num2);
+            } else {
+                System.out.println("Error: No se puede dividir entre cero.");
+                operacionValida = false;
+            }
+            break;
+        default:
+            System.out.println("Operación no válida.");
+            operacionValida = false;
+    }
+
+    if (operacionValida) {
+        System.out.println("El resultado es: " + resultado);
+    }
+    
+    teclado.close();
+}
+	
+	
+	public static int sumar(int num1, int num2 ) {
+		return num1 + num2;
+	}
+	
+	public static int restar(int num1, int num2 ) {
+		return num1 - num2;
+	}
+
+	public static int multiplicar(int num1, int num2 ) {
+		return num1 * num2;
+	}
+
+	public static int dividir(int num1, int num2 ) {
+		return num1 / num2;
+	}
+}
