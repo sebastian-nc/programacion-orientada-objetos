89399dd4a2060d9eb57d080f1a12204a59011cb1
sebastian <scsebastian1406@gmail.com>
2026-05-29 17:56:58 -0500

    actualizar estructura de carpetas

---
+++ src/semana_1/caso3/Operacion.java
@@ -0,0 +1,24 @@
+package semana_1.caso3;
+
+public class Operacion {
+	
+    public int sumar(int a, int b) {
+        return a + b;
+    }
+
+    public double sumar(double a, double b) {
+        return a + b;
+    }
+
+    public int sumar(int a, int b, int c) {
+        return a + b + c;
+    }
+    
+    public static void main(String[] args) {
+        Operacion operacion = new Operacion();
+        
+        System.out.println("12 + 50: " + operacion.sumar(12, 50));
+        System.out.println("1.7 + 4.2: " + operacion.sumar(1.7, 4.2));
+        System.out.println("5 + 4 + 15: " + operacion.sumar(5, 4, 15));
+    }
+}
