74e6a4a509f84e9507f838cd33d569f20a513418
sebastian <scsebastian1406@gmail.com>
2026-05-29 18:09:10 -0500

    actualizar operacion

--- src/semana_1/caso3/Operacion.java
+++ src/semana_1/caso3/Operacion.java
@@ -17,7 +17,7 @@ public class Operacion {
     public static void main(String[] args) {
         Operacion operacion = new Operacion();
         
-        System.out.println("12 + 50: " + operacion.sumar(12, 50));
+        System.out.println("25 + 50: " + operacion.sumar(25, 50));
         System.out.println("1.7 + 4.2: " + operacion.sumar(1.7, 4.2));
         System.out.println("5 + 4 + 15: " + operacion.sumar(5, 4, 15));
     }
